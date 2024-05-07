4698a52db37e34add6aa83dc9abe7ec477deadba
Anuj Rawat <anuj.rwt321@gmail.com>
2024-05-07 20:32:02 +0530

    current chekpoint | footer issue

---
+++ header.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="<link rel=" stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://code.jquery.com/jquery-3.7.1.js"
+        integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <title>Document</title>
+    <script>
+        jQuery(function ($) {
+            $(window).on("scroll", function () {
+                if ($(this).scrollTop() >= 200) {
+                    $(".navbar").addClass("fixed-top");
+                } else if ($(this).scrollTop() == 0) {
+                    $(".navbar").removeClass("fixed-top");
+                }
+            });
+
+            function adjustNav() {
+                var winWidth = $(window).width(),
+                    dropdown = $(".dropdown"),
+                    dropdownMenu = $(".dropdown-menu");
+
+                if (winWidth >= 768) {
+                    dropdown.on("mouseenter", function () {
+                        $(this).addClass("show").children(dropdownMenu).addClass("show");
+                    });
+
+                    dropdown.on("mouseleave", function () {
+                        $(this).removeClass("show").children(dropdownMenu).removeClass("show");
+                    });
+                } else {
+                    dropdown.off("mouseenter mouseleave");
+                }
+            }
+
+            $(window).on("resize", adjustNav);
+
+            adjustNav();
+        });
+
+    </script>
+</head>
+
+<body>
+
+
+    <header class="header-area overlay">
+        <nav class="navbar navbar-expand-md navbar-dark">
+            <div class="container d-flex justify-content-between align-items-center">
+                <img class="hfesite"
+                    src="https://www.hkmdehradun.org/wp-content/uploads/2023/10/HKM_Dehradun_Logo-removebg-preview-300x181.png"
+                    alt="HKM_Dehradun_Logo-removebg-preview" />
+
+                <button type="button" class="navbar-toggler collapsed" data-toggle="collapse" data-target="#main-nav">
+                    <span class="menu-icon-bar"></span>
+                    <span class="menu-icon-bar"></span>
+                    <span class="menu-icon-bar"></span>
+                </button>
+
+                <div id="main-nav" class="collapse navbar-collapse justify-content-center">
+                    <ul class="navbar-nav">
+                        <li><a href="#" class="nav-item nav-link active">Home</a></li>
+                        <li><a href="#" class="nav-item nav-link">About Us</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-item nav-link" data-toggle="dropdown">Services</a>
+                            <div class="dropdown-menu">
+                                <a href="#" class="dropdown-item">Dropdown Item 1</a>
+                                <a href="#" class="dropdown-item">Dropdown Item 2</a>
+                                <a href="#" class="dropdown-item">Dropdown Item 3</a>
+                            </div>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-item nav-link" data-toggle="dropdown">Portfolio</a>
+                            <div class="dropdown-menu">
+                                <a href="#" class="dropdown-item">Dropdown Item 1</a>
+                                <a href="#" class="dropdown-item">Dropdown Item 2</a>
+                                <a href="#" class="dropdown-item">Dropdown Item 3</a>
+                                <a href="#" class="dropdown-item">Dropdown Item 4</a>
+                                <a href="#" class="dropdown-item">Dropdown Item 5</a>
+                            </div>
+                        </li>
+                        <li><a href="#" class="nav-item nav-link">Contact</a></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <i class="fab fa-instagram socials"></i>
+                    <i class="fab fa-facebook-f socials"></i>
+                    <i class="fab fa-youtube socials"></i>
+                    <i class="fab fa-whatsapp socials"></i>
+                </div>
+            </div>
+        </nav>
+
+    </header>
+
+
+
+
+    <div class="banner">
+        <div class="container">
+            <h1>Bootstrap 4 Navbar</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec elit ex. Etiam elementum lectus et
+                tempor molestie.</p>
+            <a href="#content" class="button button-primary">Learn More</a>
+        </div>
+    </div>
+    </header>
+
+
+
+
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="style.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+
+
+</head>
+
+
+
+
+
+<body>
+
+
+
+    <?php include ("header.php"); ?>
+
+
+
+    <div id="content-wrapper">
+
+        <!-- ======= Hero Section ======= -->
+        <!-- <main>
+        <section id="content" class="content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec elit ex. Etiam elementum
+                            lectus et
+                            tempor molestie. Pellentesque vestibulum dui sit amet dui volutpat sollicitudin. Etiam non
+                            erat finibus,
+                            iaculis nunc vel, convallis eros. Etiam efficitur tempor dui, vitae fringilla ipsum
+                            tristique quis.
+                            Aliquam erat volutpat. Cras ullamcorper ex et viverra vulputate. Nam lectus ligula, pretium
+                            nec risus nec,
+                            ultricies fringilla mauris. Proin quis venenatis neque, iaculis porta nulla. </p>
+                    </div>
+                    <div class="col-md-4">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec elit ex. Etiam elementum
+                            lectus et
+                            tempor molestie. Pellentesque vestibulum dui sit amet dui volutpat sollicitudin. Etiam non
+                            erat finibus,
+                            iaculis nunc vel, convallis eros. Etiam efficitur tempor dui, vitae fringilla ipsum
+                            tristique quis.
+                            Aliquam erat volutpat. Cras ullamcorper ex et viverra vulputate. Nam lectus ligula, pretium
+                            nec risus nec,
+                            ultricies fringilla mauris. Proin quis venenatis neque, iaculis porta nulla. </p>
+                    </div>
+                    <div class="col-md-4">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec elit ex. Etiam elementum
+                            lectus et
+                            tempor molestie. Pellentesque vestibulum dui sit amet dui volutpat sollicitudin. Etiam non
+                            erat finibus,
+                            iaculis nunc vel, convallis eros. Etiam efficitur tempor dui, vitae fringilla ipsum
+                            tristique quis.
+                            Aliquam erat volutpat. Cras ullamcorper ex et viverra vulputate. Nam lectus ligula, pretium
+                            nec risus nec,
+                            ultricies fringilla mauris. Proin quis venenatis neque, iaculis porta nulla. </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+     </main> -->
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="image-container">
+                            <img src="assets/images/bg-img.jpg" class="img-fluid rounded" alt="Background Image">
+                        </div>
+                    </div>
+                    <div class="col-md-6 d-flex flex-column justify-content-center">
+                        <div class="content-container">
+                            <h2 class="section-title">About HKM</h2>
+                            <p class="section-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+                                elit ex. Etiam elementum
+                                lectus et
+                                tempor molestie. Pellentesque vestibulum dui sit amet dui volutpat sollicitudin. Etiam
+                                non
+                                erat finibus,
+                                iaculis nunc vel, convallis eros. Etiam efficitur tempor dui, vitae fringilla ipsum
+                                tristique quis.
+                                Aliquam erat volutpat. Cras ullamcorper ex et viverra vulputate. Nam lectus ligula,
+                                pretium
+                                nec risus nec,
+                                ultricies fringilla mauris. Proin quis venenatis neque, iaculis porta nulla.</p>
+                            <a href="#" class="btn btn-primary mt-3">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+        <!-- ********************************************************************************************************** -->
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <article class="card " style=" max-width: 75rem;">
+                        <img class="card__background" src="https://i.imgur.com/QYWAcXk.jpeg"
+                            alt="Photo of Cartagena's cathedral at the background and some colonial style houses"
+                            width="1920" height="2193" />
+                        <div class="card__content | flow">
+                            <div class="card__content--container | flow">
+                                <h2 class="card__title">Colombia</h2>
+                                <p class="card__description">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum in
+                                    labore laudantium deserunt fugiat numquam.
+                                </p>
+                            </div>
+                            <button class="card__button">Read more</button>
+                        </div>
+                    </article>
+                </div>
+                <div class="col-md-4">
+                    <article class="card">
+                        <img class="card__background" src="https://i.imgur.com/QYWAcXk.jpeg"
+                            alt="Photo of Cartagena's cathedral at the background and some colonial style houses"
+                            width="1920" height="2193" />
+                        <div class="card__content | flow">
+                            <div class="card__content--container | flow">
+                                <h2 class="card__title">Colombia</h2>
+                                <p class="card__description">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum in
+                                    labore laudantium deserunt fugiat numquam.
+                                </p>
+                            </div>
+                            <button class="card__button">Read more</button>
+                        </div>
+                    </article>
+                </div>
+            </div>
+
+
+            <div class="row" style="margin-top:36px">
+                <div class="col-md-4">
+                    <article class="card">
+                        <img class="card__background" src="https://i.imgur.com/QYWAcXk.jpeg"
+                            alt="Photo of Cartagena's cathedral at the background and some colonial style houses"
+                            width="1920" height="2193" />
+                        <div class="card__content | flow">
+                            <div class="card__content--container | flow">
+                                <h2 class="card__title">Colombia</h2>
+                                <p class="card__description">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum in
+                                    labore laudantium deserunt fugiat numquam.
+                                </p>
+                            </div>
+                            <button class="card__button">Read more</button>
+                        </div>
+                    </article>
+                </div>
+                <div class="col-md-4">
+                    <article class="card">
+                        <img class="card__background" src="https://i.imgur.com/QYWAcXk.jpeg"
+                            alt="Photo of Cartagena's cathedral at the background and some colonial style houses"
+                            width="1920" height="2193" />
+                        <div class="card__content | flow">
+                            <div class="card__content--container | flow">
+                                <h2 class="card__title">Colombia</h2>
+                                <p class="card__description">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum in
+                                    labore laudantium deserunt fugiat numquam.
+                                </p>
+                            </div>
+                            <button class="card__button">Read more</button>
+                        </div>
+                    </article>
+                </div>
+                <div class="col-md-4">
+                    <article class="card">
+                        <img class="card__background" src="https://i.imgur.com/QYWAcXk.jpeg"
+                            alt="Photo of Cartagena's cathedral at the background and some colonial style houses"
+                            width="1920" height="2193" />
+                        <div class="card__content | flow">
+                            <div class="card__content--container | flow">
+                                <h2 class="card__title">Colombia</h2>
+                                <p class="card__description">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum in
+                                    labore laudantium deserunt fugiat numquam.
+                                </p>
+                            </div>
+                            <button class="card__button">Read more</button>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+
+
+
+
+        <!-- ********************************************************************************************************** -->
+
+
+        <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css /> -->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-6">
+                    <div class="box">
+                        <img src="assets/images/bg-img.jpg">
+                        <div class="box-content">
+                            <h3 class="title">Williamson</h3>
+                            <span class="post">Web designer</span>
+                            <ul class="icon">
+                                <li><a href="#"><i class="fa fa-search"></i></a></li>
+                                <li><a href="#"><i class="fa fa-link"></i></a></li>|
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <div class="box">
+                        <img src="assets/images/bg-img.jpg">
+                        <div class="box-content">
+                            <h3 class="title">Miranda Roy</h3>
+                            <span class="post">Web developer</span>
+                            <ul class="icon">
+                                <li><a href="#"><i class="fa fa-search"></i></a></li>
+                                <li><a href="#"><i class="fa fa-link"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+        <!-- ==============End Hero =============== -->
+
+
+
+
+        <!-- Team -->
+
+
+
+
+
+
+
+
+
+
+
+        <!-- End Services Section -->
+
+
+        <!-- ======= Activites Section ======= -->
+
+        <!-- End Activites Section -->
+
+        <!-- =======track Section ======= -->
+
+
+        <!-- End track Section -->
+
+        <!-- ======= bio diversity Section ======= -->
+
+
+
+
+
+        <!-- End iodiversity Section -->
+
+        <!-- logo marque -->
+
+        <!-- logo marque end-->
+
+
+    </div>
+    <?php include ("foter.php"); ?>
+
+
+
+
+    <!-- </main>End #main -->
+
+
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <!-- <script src="assets/js/main.js"></script> -->
+
+
+    <script src="main.js" defer></script>
+
+
+</body>
+
+
+</html>
